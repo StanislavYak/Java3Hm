e0a11283db27f3f01cbda5389441391709ee4eaa
StanislavYak <karueli59@gmail.com>
2018-09-27 13:27:38 +0300

    hm2

---
+++ hm2/Homework2.java
@@ -0,0 +1,37 @@
+package ru.hm;
+
+import java.until.Scanner;
+
+public class Homework2 {
+
+        public static void echo(Object c) {
+            System.out.println(c);
+        }
+
+        public static void main(String[] args) {
+            Db db = new Db();
+            Scanner in = new Scanner(System.in);
+
+            while (true) {
+                String str = in.nextLine();
+                String[] data = str.split(" ");
+                switch (data[0]) {
+                    case "/цена":
+                        db.getProduct(data[1]);
+                        break;
+                    case "/сменитьцену":
+                        db.updatePrice(data[1], data[2]);
+                        break;
+                    case "/товарыпоцене":
+                        db.getProductsByCost(data[1], data[2]);
+                        break;
+                    default:
+                        System.out.println("Неправильная команда");
+                        break;
+                }
+                db.close();
+                break;
+            }
+        }
+    }
+}
